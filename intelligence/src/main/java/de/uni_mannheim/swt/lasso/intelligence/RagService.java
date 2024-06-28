3188ed2fde1c78b08a725e741295751719286d6b
Marcus Kessel <marcus.kessel@uni-mannheim.de>
2024-06-28 17:31:30 +0200

    #22 prototypical integration of RAG in search results using langchain4j

--- intelligence/src/main/java/de/uni_mannheim/swt/lasso/intelligence/RagService.java
+++ intelligence/src/main/java/de/uni_mannheim/swt/lasso/intelligence/RagService.java
@@ -4,9 +4,14 @@ import de.uni_mannheim.swt.lasso.core.model.CodeUnit;
 import de.uni_mannheim.swt.lasso.core.model.query.QueryResult;
 import de.uni_mannheim.swt.lasso.datasource.expansion.signature.SignatureUtils;
 import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.Metadata;
 import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.rag.DefaultRetrievalAugmentor;
+import dev.langchain4j.rag.RetrievalAugmentor;
+import dev.langchain4j.rag.content.injector.ContentInjector;
+import dev.langchain4j.rag.content.injector.DefaultContentInjector;
 import dev.langchain4j.rag.content.retriever.ContentRetriever;
 import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
 import dev.langchain4j.service.AiServices;
@@ -14,13 +19,19 @@ import dev.langchain4j.service.Result;
 import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
 import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+
+import static java.util.Arrays.asList;
 
 /**
  * Retrieval Augmented Generation.
  *
  * Ask questions based on LASSO's code searches.
+ *
+ * Based on <a href="https://docs.langchain4j.dev/tutorials/rag">RAG Tutorial</a>
  *
  * @author Marcus Kessel
  *
@@ -41,10 +52,21 @@ public class RagService {
     public Assistant create(List<CodeUnit> codeUnits) {
         ContentRetriever contentRetriever = createContentRetriever(fromQueryResult(codeUnits));
 
+        ContentInjector contentInjector = DefaultContentInjector.builder()
+                // .promptTemplate(...) // Formatting can also be changed
+                .metadataKeysToInclude(asList("id", "fullyQualifiedName", "mavenCoordinates", "type", "score"))
+                .build();
+
+        RetrievalAugmentor retrievalAugmentor = DefaultRetrievalAugmentor.builder()
+                .contentRetriever(contentRetriever)
+                .contentInjector(contentInjector)
+                .build();
+
         Assistant assistant = AiServices.builder(Assistant.class)
                 .chatLanguageModel(chatLanguageModel)
                 .chatMemory(MessageWindowChatMemory.withMaxMessages(10))
-                .contentRetriever(contentRetriever)
+                //.contentRetriever(contentRetriever)
+                .retrievalAugmentor(retrievalAugmentor)
                 .build();
 
         return assistant;
@@ -61,7 +83,21 @@ public class RagService {
     }
 
     protected Document fromCodeUnit(CodeUnit codeUnit) {
-        return Document.from(SignatureUtils.create(codeUnit).toLQL(true));
+        Map<String, Object> meta = new HashMap<>();
+        meta.put("id", codeUnit.getId());
+        meta.put("fullyQualifiedName", codeUnit.toFQName());
+        meta.put("mavenCoordinates", codeUnit.toUri());
+        meta.put("type", codeUnit.getDocType());
+        meta.put("score", codeUnit.getScore());
+
+        return Document.from(SignatureUtils.create(codeUnit).toLQL(true), Metadata.from(meta));
+    }
+
+    protected Document fromObservation() {
+        // FIXME either we need a router with two retrievers https://github.com/langchain4j/langchain4j-examples/blob/main/rag-examples/src/main/java/_3_advanced/_07_Advanced_RAG_Multiple_Retrievers_Example.java
+        // or merge into one document
+
+        return null;
     }
 
     private static ContentRetriever createContentRetriever(List<Document> documents) {
